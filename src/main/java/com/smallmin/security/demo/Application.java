cd6bc0594e85abdcced6fe33d466468a500cc754
dengyumin <eamon.deng@outlook.com>
2023-06-13 22:39:37 +0800

    init

---
+++ src/main/java/com/smallmin/security/demo/Application.java
@@ -0,0 +1,16 @@
+package com.smallmin.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dengyumin
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
